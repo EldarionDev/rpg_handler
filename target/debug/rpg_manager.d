75a0149a8fafeb0bc176087887ea888954785f29
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-09-10 16:57:58 +0200

    Removed target

--- target/debug/rpg_manager.d
+++
@@ -1,1 +0,0 @@
-/home/eldarion/Programming/rpg_manager/target/debug/rpg_manager: /home/eldarion/Programming/rpg_manager/src/armies.rs /home/eldarion/Programming/rpg_manager/src/events.rs /home/eldarion/Programming/rpg_manager/src/factions.rs /home/eldarion/Programming/rpg_manager/src/main.rs /home/eldarion/Programming/rpg_manager/src/regions.rs
