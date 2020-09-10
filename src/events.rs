962c424de387fa9900158760e257465d09f94abf
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-09-10 20:32:50 +0200

    Error :(

--- src/events.rs
+++ src/events.rs
@@ -28,7 +28,32 @@ pub mod events {
     pub mod execute {
         
         pub fn add () {
+            println!("Please enter the name of the new event!");
+            let mut event_name  =   String::new();
+            std::io::stdin().read_line(&mut event_name).expect("Error parsing input!");
 
+            println!("Please enter an event duration!");
+            let mut event_duration_string   =   String::new();
+            std::io::stdin().read_line(&mut event_duration_string).expect("Error parsing input!");
+            let event_duration: u32 =   event_duration_string.parse().unwrap();
+            
+            println!("Please enter the number of effects you want to add!");
+            let mut effect_count_string =   String::new();
+            std::io::stdin().read_line(&mut effect_count_string).expect("Error parsing input!");
+            let effect_count: u32   =   match effect_count_string.parse() {
+                Ok(effect_count) => effect_count,
+                Err(e) => {
+                    println!("Please enter a valid number! {}", e);
+                    0
+                }
+            };
+            
+            let effect_affects: Vec<String>;
+            let effect_strengths: Vec<i32>;
+            let effect_count: u32;
+            for number in 1..effect_count {
+                        
+            }
         }
 
         pub fn remove () {
